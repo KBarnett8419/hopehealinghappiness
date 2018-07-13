ad1c91a9b3fa5faf5451c0e7db440a528d09d637
Luna Star <keribarnett_7@yahoo.com>
2018-07-13 14:06:32 -0400

    changes

--- wp-content/themes/hopehealinghappiness/footer.php
+++ wp-content/themes/hopehealinghappiness/footer.php
@@ -15,8 +15,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="https://www.facebook.com/HopeHealingHappinessCounseling/" target="_blank"><img src="http://hopehealinghappinesscounseling.com/wp-content/uploads/2018/06/facebook-3-32.png" style="margin: 0 10px 0 0;"></a><a href="https://www.instagram.com/hhhcounseling/" target="_blank"><img src="http://hopehealinghappinesscounseling.com/wp-content/uploads/2018/06/instagram-32.png"></a>
-			<p>Hope, Healing & Happiness Counseling Services <br>141 South Ave, Suite 101 <br>Fanwood, NJ 07023<br>(908) 312-0417<br>Kcofone417@gmail.com<br><br>Copyright (c) 2018 Hope, Healing & Happinesss. All Rights Reserved.</p>
+			<a href="https://www.facebook.com/HopeHealingHappinessCounseling/" target="_blank"><img src="https://hopehealinghappinesscounseling.com/wp-content/uploads/2018/06/facebook-3-32.png" style="margin: 0 10px 0 0;"></a><a href="https://www.instagram.com/hhhcounseling/" target="_blank"><img src="https://hopehealinghappinesscounseling.com/wp-content/uploads/2018/06/instagram-32.png"></a>
+			<p>Hope, Healing & Happiness Counseling Services <br>141 South Ave, Suite 101 <br>Fanwood, NJ 07023<br>(908) 312-0417<br>Kcofone417@gmail.com<br><br>Copyright (c) 2018 Hope, Healing & Happinesss. All Rights Reserved.<br><span style="font-size: 0.7em;"><a href="http://barnettmarketing.org" target="_blank" style="text-decoration: none; color: #fff;">Developed by Barnett Marketing Solutions</a></span></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
